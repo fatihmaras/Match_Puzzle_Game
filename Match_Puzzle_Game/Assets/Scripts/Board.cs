eae08d2b3b596e92ac8c23f5664e63d188862300
fatihmaras <103513022+fatihmaras@users.noreply.github.com>
2023-03-01 01:43:07 +0300

    Added Tile Board

---
+++ Match_Puzzle_Game/Assets/Scripts/Board.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public GameObject bgTilePrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Setup();
+    }
+
+   
+   private void Setup() 
+   {
+        for(int x = 0; x<width; x++)
+        {
+            for(int y = 0; y<height; y++)
+            {
+                Vector2 pos = new Vector2(x,y);
+                GameObject bgTile = Instantiate(bgTilePrefab,pos,Quaternion.identity);
+                bgTile.transform.parent=transform;
+                bgTile.name="BG Tile - " + x + " , " +y ;
+            }
+
+        }
+   }
+}
