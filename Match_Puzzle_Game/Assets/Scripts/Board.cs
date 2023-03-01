d4fc45a974a9257b0b9e0f903a9a873034a7d882
fatihmaras <103513022+fatihmaras@users.noreply.github.com>
2023-03-02 01:46:17 +0300

    Refill  empty places with gem

--- Match_Puzzle_Game/Assets/Scripts/Board.cs
+++ Match_Puzzle_Game/Assets/Scripts/Board.cs
@@ -152,5 +152,40 @@ public class Board : MonoBehaviour
             nullCounter=0;
         }
 
+        StartCoroutine(FillBoardCo());
    }
+
+   private IEnumerator FillBoardCo()
+   {
+        yield return new WaitForSeconds(.5f);
+        RefillBoard();
+
+        yield return new WaitForSeconds(0.5f);
+        matchFinder.FindAllMatches();
+
+        if(matchFinder.currentMatches.Count>0)
+        {
+            yield return new WaitForSeconds(.5f);
+            DestroyMatches();
+        }
+
+   }
+
+   private void RefillBoard()
+   {
+        for(int x = 0; x<width; x++)   // control to empty spaces
+        {
+            for(int y = 0; y<height; y++)
+            {
+                if(allGems[x,y]==null)
+                {
+                    int gemToUse = Random.Range(0,gems.Length);
+                    SpawnGem(new Vector2Int(x,y), gems[gemToUse]);
+                }
+                
+            }
+        }
+   }
+
+   
 }
